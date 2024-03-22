7ee3e2ef4719656ef5065f188afef8a45623a54a
thien.ngo <thien.ngo@finviet.com.vn>
2024-03-22 15:15:13 +0700

    FEAT: add user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,22 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {   
+                name: "thienngo",
+                age: 25
+            },
+            {   
+                name: "messi",
+                age: 38
+            },
+        ]
+    })
+})
+
 module.exports = router;
